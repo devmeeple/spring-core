dc36a637771998b869b9f06302ee459fbda269b1
devmeeple <devmeeple@gmail.com>
2023-05-26 12:21:36 +0900

    새로운 할인 정책 개발

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -3,19 +3,15 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+public class RateDiscountPolicy implements DiscountPolicy{
 
-    private int disCountAmount = 1000; // 1000원 할인
-
-    // command + shift + t 테스트 케이스 만들기
+    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
-
         if(member.getGrade() == Grade.VIP) {
-            return disCountAmount;
+            return price * discountPercent / 100;
         } else {
             return 0;
         }
-
     }
 }
