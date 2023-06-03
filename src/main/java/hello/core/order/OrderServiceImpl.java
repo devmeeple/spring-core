c84881c8ecfe5f30dc2c75b1732b852672060082
devmeeple <devmeeple@gmail.com>
2023-06-03 11:59:53 +0900

    롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,20 +3,18 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    //    스프링 빈인데(Component도 포함) 생성자가 딱 1 개인 경우는 @Autowired 생략 가능하다.
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    //    스프링 빈인데(Component 도 포함) 생성자가 딱 1 개인 경우는 @Autowired 생략 가능하다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
