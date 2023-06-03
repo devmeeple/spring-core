f1bb04ba950b4e74917cdaa165ad8dc57568efa8
devmeeple <devmeeple@gmail.com>
2023-06-03 12:10:05 +0900

    조회 빈이 2개 이상 - 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,13 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-    //    스프링 빈인데(Component 도 포함) 생성자가 딱 1 개인 경우는 @Autowired 생략 가능하다.
+    //    스프링 빈인데(Component 도 포함) 생성자가 딱 1 개인 경우는 @Autowired 생략 가능하다. (자동의존관계주입)
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
