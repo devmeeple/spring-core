1234ec96c56e5842c42953e152944e80bc6c3e50
devmeeple <devmeeple@gmail.com>
2023-06-06 00:38:18 +0900

    스코프와 프록시

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,8 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+// 값이 하나라면 "request"가 가능하다. 하지만 여러개의 값을 넣어줘야 한다면 value 키 워드는 꼭 필요하다.
 public class MyLogger {
     private String uuid;
     private String requestURL;
